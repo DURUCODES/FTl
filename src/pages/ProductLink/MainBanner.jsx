a1f63249b97323a780e313665978dca97a0f73b3
KINGSLEY DURUCHUKWU <142257235+DURUCODES@users.noreply.github.com>
2025-02-19 21:37:14 -0800

    banner

--- src/pages/ProductLink/MainBanner.jsx
+++ src/pages/ProductLink/MainBanner.jsx
@@ -3,8 +3,8 @@ import banner from "./ftl2.JPG";
 
 const MainBanner = () => {
   return (
-    <div className="overflow-hidden h-screen">
-      <img src={banner} alt="Banner" className="w-full h-full cover" />
+    <div className="">
+      <img src={banner} alt="Banner" className="w-full h-full object-fit" />
     </div>
   );
 };
